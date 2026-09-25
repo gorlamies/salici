713c92513d62bbe43bb773f5610af71ab8eedb26
omismone <simone.migliazza1@gmail.com>
2026-09-25 23:29:37 +0200

    refactor: remove global exception filter and logger middleware

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,8 +2,6 @@ import { Module } from "@nestjs/common";
 import { HealthModule } from "./health/health.module";
 import { DatabaseModule } from "./database/database.module";
 import { GamesModule } from "./games/games.module";
-import { MiddlewareConsumer, NestModule } from "@nestjs/common";
-import { LoggerMiddleware } from "./common/middleware/logger.middleware";
 import { AuthModule } from "./auth/auth.module";
 
 @Module({
@@ -11,8 +9,4 @@ import { AuthModule } from "./auth/auth.module";
   controllers: [],
   providers: [],
 })
-export class AppModule implements NestModule {
-  configure(consumer: MiddlewareConsumer): void {
-    consumer.apply(LoggerMiddleware).forRoutes("*");
-  }
-}
+export class AppModule {}
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,15 +1,16 @@
-import 'reflect-metadata';
+import "reflect-metadata";
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
-import { setupSwagger } from './swagger/swagger';
-import { AllExceptionsFilter } from './common/filters/all-exceptions.filter';
-import cookieParser from 'cookie-parser';
+import { setupSwagger } from "./swagger/swagger";
+import cookieParser from "cookie-parser";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
-  app.useGlobalFilters(new AllExceptionsFilter());
-  app.enableCors({ origin: process.env.FRONTEND_URL ?? "http://localhost:5173", credentials: true })
+  app.enableCors({
+    origin: process.env.FRONTEND_URL ?? "http://localhost:5173",
+    credentials: true,
+  });
   const port = process.env.PORT ? Number(process.env.PORT) : 3000;
   setupSwagger(app);
   await app.listen(port);
